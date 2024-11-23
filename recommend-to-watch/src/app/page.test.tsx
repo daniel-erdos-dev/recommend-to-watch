14197b909de35e019f882941f560bd3326f25156
daniel-erdos-dev <daniel.erdos87@gmail.com>
2024-11-23 18:43:38 +0100

    Basic tests for Home page and SelectRegion page

---
+++ recommend-to-watch/src/app/page.test.tsx
@@ -0,0 +1,56 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "redux/configureStore";
+import Home from "./page";
+import { movieSlice } from "redux/reducers/movieReducer";
+
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      prefetch: () => null,
+    };
+  },
+}));
+
+describe("Home page tests", () => {
+  it("renders the header to the page", () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+
+    const headingElement = screen.getByRole("heading", { level: 1 });
+
+    expect(headingElement).toBeInTheDocument();
+    expect(headingElement).toHaveTextContent(
+      "Search for your favorite movie and get movie recommendations based on it!"
+    );
+  });
+
+  it("renders the input to the page", () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+
+    const searchBox = screen.getByRole("textbox");
+
+    expect(searchBox).toBeInTheDocument();
+  });
+
+  it("renders the recommend button to the page", () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+
+    const recommendButton = screen.getByRole("button");
+
+    expect(recommendButton).toBeInTheDocument();
+    expect(recommendButton).toHaveTextContent("Recommend");
+  });
+});
